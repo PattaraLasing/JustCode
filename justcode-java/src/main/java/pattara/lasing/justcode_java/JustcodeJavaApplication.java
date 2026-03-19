9acc8f326714866907309b115cbe994799fe7f56
Pattara <Pattara.Lasing@etu.unige.ch>
2026-03-19 12:24:55 +0100

    generate spring project

---
+++ justcode-java/src/main/java/pattara/lasing/justcode_java/JustcodeJavaApplication.java
@@ -0,0 +1,13 @@
+package pattara.lasing.justcode_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustcodeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustcodeJavaApplication.class, args);
+	}
+
+}
---
+++ justcode-java/src/test/java/pattara/lasing/justcode_java/JustcodeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pattara.lasing.justcode_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustcodeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
